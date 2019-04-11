9f5cd9537c764c11a27d9df4ae67201ccc030912
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2019-04-11 23:46:04 +0700

    Fixed error.

--- focalloss.py
+++ focalloss.py
@@ -18,8 +18,9 @@ class FocalLoss(nn.Module):
             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-        target = target.view(-1,1)
+        target = target.view(-1,1).to(torch.uint8)
 
+        input = torch.where(target, input, 1-input)
         logpt = self.log_sigmoid(input)
         logpt = logpt.view(-1)
         pt = Variable(logpt.data.exp())
@@ -27,8 +28,7 @@ class FocalLoss(nn.Module):
         if self.alpha is not None:
             if self.alpha.type()!=input.data.type():
                 self.alpha = self.alpha.type_as(input.data)
-            at = self.alpha.gather(0,target.data.view(-1))
-            logpt = logpt * Variable(at)
+            logpt = torch.where(target, self.alpha[0]* logpt, self.alpha[1] * logpt)
 
         loss = -1 * (1-pt)**self.gamma * logpt
         if self.size_average: return loss.mean()
